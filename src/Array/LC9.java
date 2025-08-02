c67eb97ce503a7576c57fcea1786535aa4cbbc92
Gagan41 <gagant.p100@gmail.com>
2025-08-02 15:34:13 +0530

    9. palindrome Number

---
+++ src/Array/LC9.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class LC9 {
+    public static boolean isPalindrome(int x){
+       if(x < 0 || ( x % 10 == 0 && x != 0 )) return false;
+
+       int rev = 0;
+       while(x>rev){
+           rev = rev * 10 + x % 10;
+           x /= 10;
+       }
+        return (x == rev || x == rev / 10);
+    }
+
+    public static void main(String[] args) {
+        int n = 121;
+        System.out.println(isPalindrome(n));
+    }
+}
