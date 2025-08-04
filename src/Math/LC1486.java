a48d358150decff0394409e7b08f40f47fe563a6
Gagan41 <gagant.p100@gmail.com>
2025-08-04 05:57:02 +0530

    1486. XOR Operation in an Array

---
+++ src/Math/LC1486.java
@@ -0,0 +1,32 @@
+package Math;
+
+public class LC1486 {
+    static int xorOperation(int n, int start){
+        int xor = 0;
+        for(int i=0; i<n; i++){
+            xor ^= start + 2 * i;
+        }
+        return xor;
+    }
+
+    public static void main(String[] args) {
+        int n = 5,start = 0;
+        int res = xorOperation(n,start);
+        System.out.println(res);
+    }
+}
+
+//You are given an integer n and an integer start.
+//
+//Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+//
+//Return the bitwise XOR of all elements of nums.
+//
+//
+//
+//        Example 1:
+//
+//Input: n = 5, start = 0
+//Output: 8
+//Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+//Where "^" corresponds to bitwise XOR operator.
