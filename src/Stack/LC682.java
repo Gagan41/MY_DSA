7a801669040c80392dc4ffe177254d22ae14fcad
Gagan41 <gagant.p100@gmail.com>
2025-08-05 05:30:05 +0530

    682. Baseball Game

---
+++ src/Stack/LC682.java
@@ -0,0 +1,65 @@
+package Stack;
+import java.util.*;
+
+public class LC682 {
+    static int calPoints(String[] operations) {
+        List<Integer> l = new ArrayList<>();
+
+        for(String str : operations){
+            int size = l.size();
+
+            switch (str){
+                case "+":
+                    l.add(l.get(size-1) + l.get(size -2));
+                    break;
+                case "D":
+                    l.add(2 * l.get(size - 1));
+                    break;
+                case "C":
+                    l.remove(size - 1);
+                    break;
+                default:
+                    l.add(Integer.parseInt(str));
+            }
+        }
+
+
+        int sum = 0;
+        for(int n:l) sum += n;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String[] str = {"5","2","C","D","+"};
+        int rs = calPoints(str);
+        System.out.println(rs);
+    }
+}
+
+//You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+//
+//You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+//
+//An integer x.
+//Record a new score of x.
+//'+'.
+//Record a new score that is the sum of the previous two scores.
+//        'D'.
+//Record a new score that is the double of the previous score.
+//'C'.
+//Invalidate the previous score, removing it from the record.
+//Return the sum of all the scores on the record after applying all the operations.
+//
+//The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid.
+//
+//Example 1:
+//
+//Input: ops = ["5","2","C","D","+"]
+//Output: 30
+//Explanation:
+//        "5" - Add 5 to the record, record is now [5].
+//        "2" - Add 2 to the record, record is now [5, 2].
+//        "C" - Invalidate and remove the previous score, record is now [5].
+//        "D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+//        "+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+//The total sum is 5 + 10 + 15 = 30.
