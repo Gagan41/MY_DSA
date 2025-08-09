c689c52c4a2356da29bc8decb0c12eb94e9e341c
Gagan41 <gagant.p100@gmail.com>
2025-08-09 12:01:02 +0530

    1. Two Sum updated

--- src/Array/LC1.java
+++ src/Array/LC1.java
@@ -17,6 +17,18 @@ public class LC1 {
         return new int[]{};
     }
 
+    public int[] sum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (nums[j] + nums[j - i] == target) {
+                    return new int[] { j - i, j };
+                }
+            }
+        }
+        return new int[] {};
+    }
+
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
