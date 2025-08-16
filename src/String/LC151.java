a6e0a049c829f4a96a07712967ac8b424467e679
Gagan41 <gagant.p100@gmail.com>
2025-08-16 18:38:08 +0530

    151. Reverse Words in a String

---
+++ src/String/LC151.java
@@ -0,0 +1,39 @@
+package String;
+
+public class LC151 {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i > 0) sb.append(" ");
+        }
+
+        return sb.toString();
+    }
+
+    public String reverseWord(String s) {
+        String[] words  = s.trim().split("\\s+");
+        String[] res = new String[words.length];
+        for(int i=words.length-1; i>=0; i--){
+            res[words.length - 1 - i] = words[i];
+        }
+        return String.join(" ", res);
+    }
+}
+
+//Given an input string s, reverse the order of the words.
+//
+//A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+//
+//Return a string of the words in reverse order concatenated by a single space.
+//
+//Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "the sky is blue"
+//Output: "blue is sky the"
