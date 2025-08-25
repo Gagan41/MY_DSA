8558113352e22ff32b76c408fccf7c6db2ec8d6d
Gagan41 <gagant.p100@gmail.com>
2025-08-25 18:43:12 +0530

    34. Find First and Last Position of Element in Sorted Array

---
+++ src/Searching/LC34.java
@@ -0,0 +1,58 @@
+package Searching;
+
+public class LC34 {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                r = mid - 1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                l = mid + 1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
+
+//Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+//
+//If target is not found in the array, return [-1, -1].
+//
+//You must write an algorithm with O(log n) runtime complexity.
+
+//Example 1:
+//
+//Input: nums = [5,7,7,8,8,10], target = 8
+//Output: [3,4]
+//Example 2:
+//
+//Input: nums = [5,7,7,8,8,10], target = 6
+//Output: [-1,-1]
