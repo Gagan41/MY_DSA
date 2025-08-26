a36392030e7bdb01a085fc8c33c82826cc411705
Gagan41 <gagant.p100@gmail.com>
2025-08-26 20:39:35 +0530

    231. Power of Two

--- src/Math/LC231.java
+++ src/Math/LC231.java
@@ -4,6 +4,12 @@ public class LC231 {
     public boolean isPowerOfTwo(int n) {
         return (n > 0) && (n & (n - 1)) == 0;
     }
+    //recursion function
+    static boolean isPower(int n) {
+       if(n == 1) return true;
+       else if(n == 0 || n%2 == 1) return false;
+       return isPower(n/2);
+    }
 }
 
 //Given an integer n, return true if it is a power of two. Otherwise, return false.
