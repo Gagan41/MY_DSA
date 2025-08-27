cd3716b9cf431c25a6a164b142dec2e354401098
Gagan41 <gagant.p100@gmail.com>
2025-08-27 18:31:20 +0530

    136. Single Number

---
+++ src/Array/LC136.java
@@ -0,0 +1,33 @@
+package Array;
+import java.util.*;
+
+public class LC136 {
+    static int singleNumber(int[] nums) {
+        int res = 0;
+        for(int n:nums){
+            res ^= n;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,2,1};
+        int res = singleNumber(nums);
+        System.out.println(res);
+    }
+}
+
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//
+//You must implement a solution with a linear runtime complexity and use only constant extra space.
+//Example 1:
+//
+//Input: nums = [2,2,1]
+//
+//Output: 1
+//
+//Example 2:
+//
+//Input: nums = [4,1,2,1,2]
+//
+//Output: 4
