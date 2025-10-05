3703fbaa345b71c7d5f4f8f0d160e209d0ccff61
Gagan41 <gagant.p100@gmail.com>
2025-10-05 19:32:23 +0530

    LC540. Single Element in a Sorted Array

---
+++ src/Array/LC540.java
@@ -0,0 +1,32 @@
+package Array;
+
+public class LC540 {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0, r = nums.length-1;
+        while(l<r){
+            int mid = l+(r-l)/2;
+            if(mid%2 == 1) mid--;
+            if(nums[mid]==nums[mid+1]) l = mid+2;
+            else r = mid;
+        }
+        return nums[l];
+    }
+}
+
+//You are given a sorted array consisting of only integers where every
+// element appears exactly twice, except for one element which appears exactly once.
+//
+//Return the single element that appears only once.
+//
+//Your solution must run in O(log n) time and O(1) space.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [1,1,2,3,3,4,4,8,8]
+//Output: 2
+//Example 2:
+//
+//Input: nums = [3,3,7,7,10,11,11]
+//Output: 10
