9e75f041fd6bc13e5d6eebe263c141fa2d5c1782
Gagan41 <gagant.p100@gmail.com>
2025-10-16 20:59:36 +0530

    LC263. Ugly Number

--- src/Math/LC263.java
+++ src/Math/LC263.java
@@ -15,6 +15,7 @@ public class LC263 {
     }
 }
 
+
 //An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
 //
 //Given an integer n, return true if n is an ugly number.
