33cb74ff7cce99b45b5fe5dede3010eddd6ac62f
Gagan41 <gagant.p100@gmail.com>
2025-11-18 20:10:53 +0530

    HK. Printing Math Multiplication

---
+++ src/hackerRank/HK002.java
@@ -0,0 +1,14 @@
+package hackerRank;
+import java.util.Scanner;
+
+public class HK002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1; i<=10; i++){
+            System.out.println(N+" x "+i+" = "+N*i);
+        }
+        sc.close();
+    }
+}
+
