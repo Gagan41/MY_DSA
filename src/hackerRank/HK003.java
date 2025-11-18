6b1e390007698835f93485611850ba11f5352dd5
Gagan41 <gagant.p100@gmail.com>
2025-11-18 20:21:16 +0530

    HK. Loops ||

---
+++ src/hackerRank/HK003.java
@@ -0,0 +1,26 @@
+package hackerRank;
+import java.util.*;
+
+
+public class HK003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();  // number of queries
+
+        while (q-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+
+            int sum = a;
+
+            for (int i = 0; i < n; i++) {
+                sum += (int)(Math.pow(2, i) * b);
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
