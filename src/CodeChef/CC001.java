b792af000cdd7d2358adbe707b2e47f9c26e2255
Gagan41 <gagant.p100@gmail.com>
2025-11-19 19:30:34 +0530

    CC. Printing squares, while loop

---
+++ src/CodeChef/CC001.java
@@ -0,0 +1,11 @@
+package CodeChef;
+
+public class CC001 {
+    public static void main(String[] args) {
+        int x = 1;
+        while(x <= 5){
+            System.out.print(x*x+" ");
+            x++;
+        }
+    }
+}
