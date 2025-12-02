f3ad2b0aa0cb5ed473e37a26ec10a212eacca888
Gagan41 <gagant.p100@gmail.com>
2025-12-02 16:41:23 +0530

    HK. Java IP regex

---
+++ src/hackerRank/HK_IP_Regex.java
@@ -0,0 +1,16 @@
+package hackerRank;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+
+public class HK_IP_Regex {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
---
+++ src/hackerRank/MyRegex.java
@@ -0,0 +1,7 @@
+package hackerRank;
+
+public class MyRegex {
+    public static final String pattern =
+            "^((25[0-5]|2[0-4]\\d|1\\d\\d|0?\\d\\d|0?\\d)\\.){3}"
+                    + "(25[0-5]|2[0-4]\\d|1\\d\\d|0?\\d\\d|0?\\d)$";
+   }
