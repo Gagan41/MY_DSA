dcb01c10b4ab75b8b369d359f917bb911e58277d
Gagan41 <gagant.p100@gmail.com>
2025-12-18 09:05:44 +0530

    LC50. Pow(x, n)

---
+++ src/Math/LC50.java
@@ -0,0 +1,28 @@
+package Math;
+
+public class LC50 {
+    static double myPow(double x, int n){
+        long N = n;
+        if(N < 0){
+            x = 1/x;
+            N = -N;
+        }
+
+        double res = 1.0;
+        while(N > 0){
+            if(N % 2 == 1){
+                res *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        double x = 2.00000;
+        int n = 10;
+        System.out.println(myPow(x,n));
+    }
+}
