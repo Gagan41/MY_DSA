10a28b573c7ce5cd2946bb9960382aa6def7a955
Gagan41 <gagant.p100@gmail.com>
2025-12-31 20:14:41 +0530

    LC1365. How Many Numbers Are Smaller Than the Current Number

---
+++ src/Array/LC1365.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class LC1365 {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] arr = new int[nums.length];
+
+        for(int i=0; i<nums.length; i++){
+            int count = 0;
+            for(int j=0; j<nums.length; j++){
+                if(nums[j] < nums[i]){
+                    count++;
+                }
+            }
+            arr[i] = count;
+        }
+
+        return arr;
+    }
+}
