a80627663e3bd311b1533c85b5423a8b8433e04d
Gagan41 <gagant.p100@gmail.com>
2026-01-10 20:51:41 +0530

    LC215. Kth Largest Element in an Array

---
+++ src/Sorting/LC215.java
@@ -0,0 +1,9 @@
+package Sorting;
+import java.util.*;
+
+public class LC215 {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[(nums.length)-k];
+    }
+}
