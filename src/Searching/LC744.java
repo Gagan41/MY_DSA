390e7649c9a825b3f3c23aea3909545bb46fff98
Gagan41 <gagant.p100@gmail.com>
2026-02-01 22:04:13 +0530

    LC744. Find Smallest Letter Greater Than Target

---
+++ src/Searching/LC744.java
@@ -0,0 +1,25 @@
+package Searching;
+
+public class LC744 {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0, right = letters.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return letters[left % letters.length];
+    }
+
+    //for(int i=0;i<letters.length;i++){
+    //            if(target<letters[i])
+    //            return letters[i];
+    //        }
+    //        return letters[0];
+}
