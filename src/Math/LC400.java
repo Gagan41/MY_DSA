23eb0d656fe2a38a6504744cc76ee1a309e92e90
Gagan41 <gagant.p100@gmail.com>
2026-02-06 08:02:22 +0530

    LC400. Nth Digit

---
+++ src/Math/LC400.java
@@ -0,0 +1,21 @@
+package Math;
+
+public class LC400 {
+    public int findNthDigit(int n) {
+        long digit = 1;
+        long count = 9;
+        long start = 1;
+
+        while(n > digit * count){
+            n -= digit * count;
+            digit++;
+            count *= 10;
+            start *= 10;
+        }
+
+        long num = start + (n -1) / digit;
+        int di = (int)((n-1) % digit);
+
+        return String.valueOf(num).charAt(di) - '0';
+    }
+}
