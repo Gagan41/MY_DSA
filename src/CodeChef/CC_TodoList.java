66dd7c5d6a8e2f7efe95c0124f5f6cf2ae118630
Gagan41 <gagant.p100@gmail.com>
2026-02-09 20:26:50 +0530

    CC. Chef TodoList

---
+++ src/CodeChef/CC_TodoList.java
@@ -0,0 +1,27 @@
+package CodeChef;
+import java.util.*;
+
+public class CC_TodoList {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+
+            for(int i=0; i<n; i++){
+                arr[i] = sc.nextInt();
+            }
+
+            int count = 0;
+            for(int num:arr){
+                if(num >= 1000) count++;
+            }
+
+            System.out.println(count);
+        }
+
+    }
+}
