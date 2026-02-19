a6e541b0b2a8134d85651b086559c3815b26ce12
Gagan41 <gagant.p100@gmail.com>
2026-02-19 08:38:03 +0530

    CC. Jenga

---
+++ src/CodeChef/CC_Jenga.java
@@ -0,0 +1,23 @@
+package CodeChef;
+import java.util.*;
+
+public class CC_Jenga {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+
+            if(n <= x && x % n == 0){
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+
+        }
+
+    }
+}
