b1c89ba107e26902f6e8cf9ac368c27479afd29a
Gagan41 <gagant.p100@gmail.com>
2026-02-20 08:26:02 +0530

    CC. SeatNumber

---
+++ src/CodeChef/CC_SeatNumber.java
@@ -0,0 +1,25 @@
+package CodeChef;
+import java.util.*;
+
+public class CC_SeatNumber {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+
+            if(n >=1 && n <= 10){
+                System.out.println("Lower Double");
+            } else if(n >=11 && n <= 15){
+                System.out.println("Lower Single");
+            } else if(n >= 16 && n <=25){
+                System.out.println("Upper Double");
+            } else {
+                System.out.println("Upper Single");
+            }
+        }
+
+    }
+}
