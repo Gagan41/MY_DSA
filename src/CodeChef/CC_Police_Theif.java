2382cfb67066b24b8268b43a76c671ee9c7dadc0
Gagan41 <gagant.p100@gmail.com>
2026-02-24 08:20:59 +0530

    CC. Police & Thief

---
+++ src/CodeChef/CC_Police_Theif.java
@@ -0,0 +1,22 @@
+package CodeChef;
+import java.util.*;
+
+public class CC_Police_Theif {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if(a >=b ){
+                System.out.println(a-b);
+            } else {
+                System.out.println(b-a);
+            }
+        }
+
+    }
+}
