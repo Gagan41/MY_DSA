ebddba6c51f04acad02c3cd334be3789a49d86e3
Gagan41 <gagant.p100@gmail.com>
2026-03-06 08:09:01 +0530

    CC. Candy Distribution

---
+++ src/CodeChef/CC_Candy_Distribution.java
@@ -0,0 +1,21 @@
+package CodeChef;
+import java.util.*;
+
+public class CC_Candy_Distribution {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            if(n % m == 0 && (n/m) % 2 == 0){
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
