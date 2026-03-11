422c84e5e204af48051924c4c367413112644e5a
Gagan41 <gagant.p100@gmail.com>
2026-03-11 08:48:45 +0530

    LC1009. Complement of Base 10 Integer

---
+++ src/Bit/LC1009.java
@@ -0,0 +1,16 @@
+package Bit;
+
+public class LC1009 {
+    public int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+        int mask = 1;
+
+        while(mask <= n){
+            mask <<= 1;
+        }
+
+        mask -= 1;
+
+        return n ^ mask;
+    }
+}
