cb0bedca9e28ee2b48e10edee12304c2e33ad1c5
Gagan41 <gagant.p100@gmail.com>
2026-03-14 08:49:51 +0530

    LC946. Validate Stack Sequences

---
+++ src/Stack/LC946.java
@@ -0,0 +1,20 @@
+package Stack;
+import java.util.Stack;
+
+public class LC946 {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+
+        for(int n:pushed){
+            stack.push(n);
+
+            while(!stack.isEmpty() && stack.peek() == popped[i]){
+                stack.pop();
+                i++;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
