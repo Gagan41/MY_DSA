67f8ef60cb33f2e6552981c05910154b22364bfe
Gagan41 <gagant.p100@gmail.com>
2026-03-25 08:32:43 +0530

    CC. DecInc

---
+++ src/CodeChef/CC_DecInc.java
@@ -0,0 +1,18 @@
+package CodeChef;
+import java.util.Scanner;
+
+public class CC_DecInc {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(n % 4 == 0){
+            System.out.println(++n);
+        } else{
+            System.out.println(--n);
+        }
+
+    }
+}
