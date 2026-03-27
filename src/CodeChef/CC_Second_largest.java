3c5f695d5d3d0c55c8611aded2120d32a77a4429
Gagan41 <gagant.p100@gmail.com>
2026-03-27 07:56:52 +0530

    CC. Second Largest.

---
+++ src/CodeChef/CC_Second_largest.java
@@ -0,0 +1,22 @@
+package CodeChef;
+import java.util.*;
+
+public class CC_Second_largest {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int[] arr = new int[3];
+
+            for(int i=0; i<3; i++){
+                arr[i] = sc.nextInt();
+            }
+
+            Arrays.sort(arr);
+            System.out.println(arr[1]);
+        }
+
+    }
+}
