8ab6945b3159c61a4befc2c0808517d80a515ad6
Gagan41 <gagant.p100@gmail.com>
2026-04-07 19:58:28 +0530

    LC739. Daily Temperatures

--- src/Stack/LC739.java
+++ src/Stack/LC739.java
@@ -6,7 +6,7 @@ public class LC739 {
         int n = temperatures.length;
         int[] res = new int[n];
 
-        Stack<Integer> stack = new Stack<>(); // stores indices
+        Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < n; i++) {
             while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
