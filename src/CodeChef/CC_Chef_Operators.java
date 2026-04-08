1b75830d112bdba04053a32495151693391f552b
Gagan41 <gagant.p100@gmail.com>
2026-04-08 08:46:39 +0530

    CC. Chef and Operators

---
+++ src/CodeChef/CC_Chef_Operators.java
@@ -0,0 +1,24 @@
+package CodeChef;
+import java.util.*;
+
+public class CC_Chef_Operators {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int a = sc.nextInt();
+            int b  = sc.nextInt();
+
+            if(a < b){
+                System.out.println("<");
+            } else if(a == b){
+                System.out.println("=");
+            } else {
+                System.out.println(">");
+            }
+        }
+
+    }
+}
