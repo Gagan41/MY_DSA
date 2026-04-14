f75f4a5b0d445a7bfabf84998f69baf1e6397abd
Gagan41 <gagant.p100@gmail.com>
2026-04-14 09:15:53 +0530

    LC168. Excel Sheet Column Title

---
+++ src/Math/LC168.java
@@ -0,0 +1,18 @@
+package Math;
+
+public class LC168 {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+
+        while(columnNumber > 0){
+            columnNumber--;
+
+            int rem = columnNumber % 26;
+            sb.append((char)('A' + rem));
+
+            columnNumber /= 26;
+        }
+
+        return sb.reverse().toString();
+    }
+}
