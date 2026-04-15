046e86454f15e6349ed6af98a1fc2d1c9f5304b0
Gagan41 <gagant.p100@gmail.com>
2026-04-15 08:36:04 +0530

    LC171. Excel Sheet Column Number

---
+++ src/Math/LC171.java
@@ -0,0 +1,16 @@
+package Math;
+
+public class LC171 {
+    public int titleToNumber(String columnTitle){
+        int res = 0;
+
+        for(int i=0; i<columnTitle.length(); i++){
+            char ch = columnTitle.charAt(i);
+            int val = ch - 'A' + 1;
+
+            res = res * 26 + val;
+        }
+
+        return res;
+    }
+}
