5b3c1f28317ab9d7a3d1cefddfd9efc0fd87a765
Gagan41 <gagant.p100@gmail.com>
2026-04-19 08:45:52 +0530

    LC1855. Maximum Distance Between a Pair of Values

---
+++ src/Array/LC1855.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class LC1855 {
+    public int maxDistance(int[] nums1, int[] nums2) {
+        int i= 0, j = 0, maxd = 0;
+
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] <= nums2[j]){
+                maxd = Math.max(maxd, j-i);
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return maxd;
+    }
+}
