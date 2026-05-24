4e1e5a3d3366e2e8c7c06d9b2c52633ffcf61cf9
Gagan41 <gagant.p100@gmail.com>
2026-05-24 07:51:48 +0530

    LC447. Number of Boomerangs

---
+++ src/Map/LC447.java
@@ -0,0 +1,28 @@
+package Map;
+import java.util.*;
+
+public class LC447 {
+    public int numberOfBoomerangs(int[][] points) {
+        int res = 0;
+
+        for(int i=0; i<points.length; i++){
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+            for(int j=0; j<points.length; j++){
+                if(i == j) continue;
+
+                int dx = points[i][0] - points[j][0];
+                int dy = points[i][1] - points[j][1];
+
+                int dist = dx * dx + dy * dy;
+                map.put(dist, map.getOrDefault(dist, 0) + 1);
+            }
+
+            for(int count:map.values()){
+                res += count * (count - 1);
+            }
+        }
+
+        return res;
+    }
+}
