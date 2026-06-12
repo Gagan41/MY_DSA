85f884efea45840aff1c1eddc0f7bed475e136c7
Gagan41 <gagant.p100@gmail.com>
2026-06-12 08:04:41 +0530

    LC1991. Find the Middle Index in Array

---
+++ src/PrefixSum/LC1991.java
@@ -0,0 +1,25 @@
+package PrefixSum;
+
+public class LC1991 {
+    public int findMiddleIndex(int[] nums) {
+        int tot = 0;
+
+        for(int n:nums){
+            tot += n;
+        }
+
+        int lsum = 0;
+
+        for(int i=0; i<nums.length; i++){
+            int rsum = tot - lsum - nums[i];
+
+            if(lsum == rsum){
+                return i;
+            }
+
+            lsum += nums[i];
+        }
+
+        return -1;
+    }
+}
