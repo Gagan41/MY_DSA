468316630144478e8e6dc492bbbe2ecf59faced3
Gagan41 <gagant.p100@gmail.com>
2026-07-10 07:33:26 +0530

    LC275. H-Index II

---
+++ src/Searching/LC275.java
@@ -0,0 +1,20 @@
+package Searching;
+
+public class LC275 {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int l = 0, r = n - 1;
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+
+            if(citations[mid] >= n - mid){
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return n - l;
+    }
+}
