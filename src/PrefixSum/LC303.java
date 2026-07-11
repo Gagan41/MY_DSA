8c893c443843510b03719f39df6d212e0dcefbf9
Gagan41 <gagant.p100@gmail.com>
2026-07-11 08:51:20 +0530

    LC303. Range Sum Query - Immutable

---
+++ src/PrefixSum/LC303.java
@@ -0,0 +1,29 @@
+package PrefixSum;
+
+public class LC303 {
+
+    private int[] prefix;
+
+    public LC303(int[] nums) {
+        int n = nums.length;
+        prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 0, 3, -5, 2, -1};
+
+        LC303 obj = new LC303(nums);
+
+        System.out.println(obj.sumRange(0, 2)); // 1
+        System.out.println(obj.sumRange(2, 5)); // -1
+        System.out.println(obj.sumRange(0, 5)); // -3
+    }
+}
