737f07fad782117bb8a59cf0ded87dbcde93a59c
Gagan41 <gagant.p100@gmail.com>
2026-07-27 19:50:01 +0530

    Lc1464. Maximum Product of Two Elements in an Array

---
+++ src/Math/LC1464.java
@@ -0,0 +1,18 @@
+package Math;
+
+public class LC1464 {
+    public int maxProduct(int[] nums) {
+        int m1 = 0, m2 = 0;
+
+        for(int n:nums){
+            if(n > m1){
+                m2 = m1;
+                m1 = n;
+            } else if(n > m2){
+                m2 = n;
+            }
+        }
+
+        return (m1 - 1) * (m2 - 1);
+    }
+}
