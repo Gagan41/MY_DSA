2ba14658fb047197e5074096ebac9b3236645cde
Gagan41 <gagant.p100@gmail.com>
2026-07-28 08:03:22 +0530

    LC3517. Smallest Palindromic Rearrangement I

---
+++ src/String/LC3157.java
@@ -0,0 +1,35 @@
+package String;
+
+public class LC3157 {
+    public String smallestPalindrome(String s) {
+        int[] freq = new int[26];
+
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
+        }
+
+        StringBuilder l = new StringBuilder();
+        char mid = 0;
+
+        for(int i=0; i<26; i++){
+            if((freq[i] & 1) == 1){
+                mid = (char)('a'+i);
+            }
+
+            for(int j=0; j<freq[i]/2; j++){
+                l.append((char) ('a'+i));
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        ans.append(l);
+
+        if(mid != 0){
+            ans.append(mid);
+        }
+
+        ans.append(new StringBuilder(l).reverse());
+
+        return ans.toString();
+    }
+}
