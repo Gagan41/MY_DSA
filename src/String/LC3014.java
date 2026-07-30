06f4c6961887f6740952e2ecec77ad8802a03dee
Gagan41 <gagant.p100@gmail.com>
2026-07-30 08:38:04 +0530

    LC3014. Minimum Number of Pushes to Type Word I

---
+++ src/String/LC3014.java
@@ -0,0 +1,13 @@
+package String;
+
+public class LC3014 {
+    public int minimumPushes(String word) {
+        int ans = 0;
+
+        for(int i=0; i<word.length(); i++){
+            ans += i / 8 + 1;
+        }
+
+        return ans;
+    }
+}
