b3dfa885bd5788805ea0395c54f80fe86424fabd
Gagan41 <gagant.p100@gmail.com>
2026-08-08 11:53:41 +0530

    LC389. Find the Difference

---
+++ src/String/LC389.java
@@ -0,0 +1,17 @@
+package String;
+
+public class LC389 {
+    public char findTheDifference(String s, String t) {
+        char res = 0;
+
+        for(char ch:s.toCharArray()){
+            res ^= ch;
+        }
+
+        for(char ch:t.toCharArray()){
+            res ^= ch;
+        }
+
+        return res;
+    }
+}
