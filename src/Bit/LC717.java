c3bdabb1665ea0333c3b5846810af0844cb3c5f0
Gagan41 <gagant.p100@gmail.com>
2026-08-12 20:52:58 +0530

    Lc717. 1-bit and 2-bit Characters

---
+++ src/Bit/LC717.java
@@ -0,0 +1,17 @@
+package Bit;
+
+public class LC717 {
+    public boolean isOneBitCharacter(int[] bits) {
+        int i = 0;
+
+        while(i < bits.length - 1){
+            if(bits[i] == 1){
+                i += 2;
+            } else {
+                i += 1;
+            }
+        }
+
+        return i == bits.length - 1;
+    }
+}
