b3360f8c42c9bcdbaf5840e28f88e785f55b186c
Gagan41 <gagant.p100@gmail.com>
2026-08-14 19:09:09 +0530

    LC3090. Maximum Length Substring With Two Occurrences

---
+++ src/String/LC3090.java
@@ -0,0 +1,23 @@
+package String;
+
+public class LC3090 {
+    public int maximumLengthSubstring(String s) {
+        int[] freq = new int[26];
+        int l = 0;
+        int maxlen = 0;
+
+        for(int r = 0; r<s.length(); r++){
+            char ch = s.charAt(r);
+            freq[ch - 'a']++;
+
+            while(freq[ch - 'a'] > 2){
+                freq[s.charAt(l) - 'a']--;
+                l++;
+            }
+
+            maxlen = Math.max(maxlen, r-l+1);
+        }
+
+        return maxlen;
+    }
+}
