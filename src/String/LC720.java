deca50fc5cb7ca22f830b198c6440698049afbb6
Gagan41 <gagant.p100@gmail.com>
2026-08-15 10:05:13 +0530

    LC720. Longest Word in Dictionary

---
+++ src/String/LC720.java
@@ -0,0 +1,23 @@
+package String;
+import java.util.*;
+
+public class LC720 {
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+
+        Set<String> set =  new HashSet<>();
+        String ans = "";
+
+        for(String w:words){
+            if(w.length() == 1 || set.contains(w.substring(0, w.length() - 1))){
+                set.add(w);
+
+                if(w.length() > ans.length()){
+                    ans = w;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
