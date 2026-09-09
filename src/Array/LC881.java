0ce7869cd9792266b059e83048d55b1ef7f02d34
Gagan41 <gagant.p100@gmail.com>
2026-09-09 20:40:16 +0530

    LC881. Boats to Save People

---
+++ src/Array/LC881.java
@@ -0,0 +1,21 @@
+package Array;
+import java.util.*;
+
+public class LC881 {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int l = 0, r = people.length-1, boats = 0;
+
+        while(l <= r){
+            if(people[l] + people[r] <= limit){
+                l++;
+            }
+
+            r--;
+            boats++;
+        }
+
+        return boats;
+    }
+}
