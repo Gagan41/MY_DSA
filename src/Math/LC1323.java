b7527fa34426df7e5b15a4e42813ab92909d9f5e
Gagan41 <gagant.p100@gmail.com>
2026-09-18 11:47:47 +0530

    LC1323. Maximum 69 Number

---
+++ src/Math/LC1323.java
@@ -0,0 +1,16 @@
+package Math;
+
+public class LC1323 {
+    public int maximum69Number (int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+
+        for(int i=0; i<digits.length; i++){
+            if(digits[i] == '6'){
+                digits[i] = '9';
+                break;
+            }
+        }
+
+        return Integer.parseInt(new String(digits));
+    }
+}
